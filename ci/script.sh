c07f726b570483116dc89a24e66a4cb1ee0e7962
Gerd Zellweger <mail@gerdzellweger.com>
2019-09-09 21:48:24 -0700

    Add link

--- ci/script.sh
+++ ci/script.sh
@@ -15,7 +15,7 @@ main() {
     cross test --target $TARGET --features utest
     cross test --target $TARGET --release --features utest
 
-    # Run KVM tests: Disabled for now since travis doesn't support KVM :/
+    # Run KVM tests: Disabled for now since travis doesn't support KVM :/ (https://travis-ci.community/t/add-kvm-support/1406)
     # RUSTFLAGS="-C relocation-model=dynamic-no-pic -C code-model=kernel" RUST_BACKTRACE=1 cross test --target $TARGET --features vmtest
 }
 
