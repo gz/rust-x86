b5e83dabc384fcaa9e59d8faf29a587ca732362d
Gerd Zellweger <mail@gerdzellweger.com>
2020-11-07 13:05:35 -0800

    Enable deadline mode in x2apic properly.

--- src/apic/x2apic.rs
+++ src/apic/x2apic.rs
@@ -89,14 +89,14 @@ impl ApicControl for X2APIC {
     fn tsc_enable(&mut self, vector: u8) {
         unsafe {
             let mut lvt: u64 = rdmsr(IA32_X2APIC_LVT_TIMER);
-            // Set vector
             lvt &= !0xff;
             lvt |= vector as u64;
+
             // Unmask timer IRQ
             lvt.set_bit(16, false);
             // Enable TSC deadline mode
             lvt.set_bit(17, false);
-            lvt.set_bit(18, false);
+            lvt.set_bit(18, true);
             wrmsr(IA32_X2APIC_LVT_TIMER, lvt);
         }
     }
